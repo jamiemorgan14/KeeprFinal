395debeb07243e45f1aec74f0ead77be2d041c72
Jamie <jmorgan3475@gmail.com>
2019-04-16 09:25:06 -0600

    card-columns fixed

--- wwwsrc/src/views/Home.vue
+++ wwwsrc/src/views/Home.vue
@@ -78,15 +78,52 @@
   }
 
   .card {
-    z-index: -1
+    z-index: -1;
+    display: inline-block
   }
 
   .clickable-card {
     z-index: 1
   }
 
-  .card-columns {
-    column-count: 4
+  @media (min-width: 200px) {
+    .card-columns {
+      -webkit-column-count: 2;
+      -moz-column-count: 2;
+      column-count: 2;
+    }
+  }
+
+  @media (min-width: 34em) {
+    .card-columns {
+      -webkit-column-count: 2;
+      -moz-column-count: 2;
+      column-count: 2;
+    }
+  }
+
+  @media (min-width: 48em) {
+    .card-columns {
+      -webkit-column-count: 3;
+      -moz-column-count: 3;
+      column-count: 3;
+    }
+  }
+
+  @media (min-width: 62em) {
+    .card-columns {
+      -webkit-column-count: 5;
+      -moz-column-count: 5;
+      column-count: 5;
+    }
+  }
+
+  @media (min-width: 75em) {
+    .card-columns {
+      -webkit-column-count: 5;
+      -moz-column-count: 5;
+      column-count: 5;
+    }
   }
 
   .modal-enter,
